cefc37cb72c2b080b61ae9787dcc5c3b805bf418
Elerin Laur <elerinlaur8@gmail.com>
2021-10-14 22:26:41 +0300

    add new projects

---
+++ MoreLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MoreLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt sisestada kasutajatunnus ja salasõna
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna on "admin1234"
+            //siis konsoolis kuvatakse "Tere tulemast!";
+            //muul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti."
+
+            Console.WriteLine("Sisesta kasutajatunnus:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta salasõna:");
+            string userPassword = Console.ReadLine();
+
+            if(userName == "admin" && userPassword == "admin1234")
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Vale kasutajatunnus või salasõna. Proovi uuesti.");
+            }
+        }
+    }
+}
